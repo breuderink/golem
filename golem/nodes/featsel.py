f728ecdd0cca6da57f1052e2a955a16175e4bc86
Boris Reuderink <b.reuderink@gmail.com>
2009-02-09 09:23:08 +0100

    Added support for #features selection in AUCFilter, added unit tests.

--- golem/nodes/featsel.py
+++ golem/nodes/featsel.py
@@ -2,10 +2,11 @@ import logging
 import numpy as np
 from golem import DataSet, helpers
 
-log = logging.getLogger('AUCFilter')
+auc_log = logging.getLogger('AUCFilter')
 class AUCFilter:
-  def __init__(self, min_auc = .6):
+  def __init__(self, min_auc = .6, nfeatures=None):
     self.min_auc = min_auc
+    self.nfeatures = nfeatures
 
   def train(self, d):
     assert(d.nclasses == 2)
@@ -15,17 +16,23 @@ class AUCFilter:
     for fi in xrange(d.nfeatures):
       scores = d.xs[:, fi]
       aucs[fi] = helpers.auc(scores, labels)
-    log.debug('Found AUCs for features: %s' % aucs)
-    self.feat_bool = np.logical_or(aucs > min_auc, aucs < 1 - min_auc)
-    log.info('Keeping %d%% of the features' % (100 * np.mean(self.feat_bool)))
-    
+    auc_log.debug('Found AUCs for features: %s' % aucs)
+
+    if self.nfeatures == None:
+      # threshold filter
+      self.feat_bool = np.logical_or(aucs > min_auc, aucs < 1 - min_auc)
+      if (self.feat_bool == False).all():
+        auc_log.warning('No features found above threshold.')
+    else:
+      # pick best nfeatures
+      self.feat_bool = np.zeros(aucs.size, bool)
+      self.feat_bool[aucs.argsort()[:self.nfeatures]] = True
+
+    auc_log.info('Keeping %d%% of the features' % (100 * self.feat_bool.mean()))
     self.cl_lab = d.cl_lab
     self.aucs = aucs
 
   def test(self, d):
     feat_lab = [d.feat_lab[i] for i in range(len(d.feat_lab)) \
       if self.feat_bool[i]]
-    print feat_lab
     return DataSet(xs=d.xs[:, self.feat_bool], feat_lab=feat_lab, default=d)
-
-log = logging.getLogger('@@DUMMY')
--- tests/testfeatsel.py
+++ tests/testfeatsel.py
@@ -8,7 +8,6 @@ class TestAUCFilter(unittest.TestCase):
     d = golem.data.gaussian_dataset([200, 200])
     self.d = golem.DataSet(
       xs=np.hstack([d.xs, np.random.random((d.ninstances, 30))]), ys=d.ys)
-    print self.d
 
   def testAUCFilter(self):
     d = self.d
@@ -17,3 +16,38 @@ class TestAUCFilter(unittest.TestCase):
     d2 = n.test(d)
     self.assert_(d2.nfeatures == 2)
     self.assert_(n.feat_bool[:2].all())
+  
+  def testAUCFilterStrong(self):
+    d = self.d
+    n = golem.nodes.featsel.AUCFilter(min_auc=.8)
+    n.train(d)
+    d2 = n.test(d)
+    self.assert_(d2.nfeatures == 1)
+    self.assert_(n.feat_bool[:1].all())
+
+  def testAUCnfeat(self):
+    d = self.d
+    for nf in range(d.nfeatures):
+      n = golem.nodes.featsel.AUCFilter(nfeatures=nf)
+      n.train(d)
+      d2 = n.test(d)
+      self.assertEqual(d2.nfeatures, nf)
+      if nf <= 2:
+        self.assert_(n.feat_bool[:nf].all())
+
+  def testAUCFilterTooStrong(self):
+    d = self.d
+    n = golem.nodes.featsel.AUCFilter(min_auc=1)
+    n.train(d)
+    d2 = n.test(d)
+    self.assert_(d2.nfeatures == 0)
+    self.assertFalse(n.feat_bool.all())
+
+  def testAUCFilterSymmetric(self):
+    d = self.d
+    dn = golem.DataSet(xs=-d.xs, default=d)
+    n = golem.nodes.featsel.AUCFilter()
+    n.train(d)
+    nn = golem.nodes.featsel.AUCFilter()
+    nn.train(dn)
+    self.assert_((n.feat_bool == nn.feat_bool).all())
