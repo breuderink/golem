d6a04b8e36fa8d36d8c4959aee30be4a8896a64d
Boris Reuderink <b.reuderink@gmail.com>
2009-07-02 12:33:36 +0200

    Added regularization parameter to RDA + visual tests.

--- golem/nodes/rda.py
+++ golem/nodes/rda.py
@@ -3,24 +3,38 @@ from scipy import stats
 from .. import DataSet
 
 class RDA:
-  def __init__(self, alpha=0, beta=1):
-    self.alpha = alpha
-    self.beta = beta
+  def __init__(self, alpha=.3, beta=.3):
+    '''
+    Regularized Discriminant Analysis, Alpaydin, p.98, Eq. 5.29:
+    S_i^{'} = \alpha \sigma^2I + \beta S + (1 - \alpha - \beta)S_i
+    
+    alpha = beta = 0 results in a quadratic classfier,
+    alpha = 0, beta = 1 results in a linear classifier,
+    alpha = 1, beta = 0 results in a nearest mean classifier.
+    '''
+    self.alpha = float(alpha)
+    self.beta = float(beta)
 
   def train(self, d):
     self.means = means = []
-    self.covs = covs = []
+    covs = []
     self.priors = np.asarray(d.ninstances_per_class) / float(d.ninstances)
     for ci in range(d.nclasses):
       cd = d.get_class(ci)
       means.append(np.mean(cd.xs, axis=0))
       covs.append(np.cov(cd.xs, rowvar=False))
 
+    a, b = self.alpha, self.beta
+    ss = np.var(d.xs)
+    S = np.cov(d.xs, rowvar=False)
+    self.covs = [a * ss * np.eye(d.nfeatures) + b * S + 
+      (1 - a - b) * Si for Si in covs]
+
   def test(self, d):
     '''Ouput log(p(x | class_i))'''
     xs = []
     for (ci, (m, S, P)) in enumerate(zip(self.means, self.covs, self.priors)):
-      # (1) Alpaydin, p.93, eq 5.20:
+      # (1) Alpaydin, p.93, Eq 5.20:
       # g_i(x) = x^TW_ix + w_i^T x + w_{i0}
       # where: 
       # W_i = -\frac{1}{2}S_i^{-1}
--- golem/tests/testrda.py
+++ golem/tests/testrda.py
@@ -6,17 +6,47 @@ from .. import plots, cv, loss
 
 class TestRDA(unittest.TestCase):
   def setUp(self):
-    self.d = gaussian_dataset([100, 5, 30])
+    self.d = gaussian_dataset([100, 100, 100])
+    #self.d = gaussian_dataset([100, 20, 10])
 
   def test_qdc(self):
-    c = RDA()
-    self.assert_(loss.mean_std(loss.accuracy, cv.rep_cv(self.d, c))[0] > .95)
+    c = RDA(alpha=0, beta=0)
+    self.assert_(loss.mean_std(loss.accuracy, cv.rep_cv(self.d, c))[0] > .90)
 
-  def test_visual(self):
-    c = RDA()
+  def test_visual_rda(self):
+    c = RDA(alpha=0, beta=.5)
     c.train(self.d)
 
     pylab.clf()
     plots.scatter_plot(self.d)
     plots.plot_classifier_hyperplane(c)
     pylab.savefig('rda.eps')
+
+
+  def test_visual_qda(self):
+    c = RDA(alpha=0, beta=0)
+    c.train(self.d)
+
+    pylab.clf()
+    plots.scatter_plot(self.d)
+    plots.plot_classifier_hyperplane(c)
+    pylab.savefig('qda.eps')
+
+  def test_visual_lda(self):
+    c = RDA(alpha=0, beta=1)
+    c.train(self.d)
+
+    pylab.clf()
+    plots.scatter_plot(self.d)
+    plots.plot_classifier_hyperplane(c)
+    pylab.savefig('lda.eps')
+
+  def test_visual_nm(self):
+    c = RDA(alpha=1, beta=0)
+    c.train(self.d)
+
+    pylab.clf()
+    plots.scatter_plot(self.d)
+    pylab.axis('equal')
+    plots.plot_classifier_hyperplane(c)
+    pylab.savefig('nm.eps')
