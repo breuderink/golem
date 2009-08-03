16294356a538bbae9060d36fdeefd334b59ca768
Boris Reuderink <b.reuderink@gmail.com>
2009-08-03 11:13:00 +0200

    Added axis argument to loss.mean_std. Reduced 'dataset' arg to 'd' in loss.py

--- golem/loss.py
+++ golem/loss.py
@@ -2,30 +2,30 @@ import numpy as np
 from dataset import DataSet
 import helpers
 
-def class_loss(dataset):
-  hm = helpers.hard_max(dataset.xs)
-  ys = dataset.ys
+def class_loss(d):
+  hm = helpers.hard_max(d.xs)
+  ys = d.ys
   loss = np.where(np.sum(np.abs(hm - ys), axis=1).reshape(-1, 1), 
     np.ones((ys.shape[0], 1)), np.zeros((ys.shape[0], 1)))
   return loss
 
-def accuracy(dataset):
-  return 1 - np.mean(class_loss(dataset))
+def accuracy(d):
+  return 1 - np.mean(class_loss(d))
 
-def confusion_matrix(dataset):
+def confusion_matrix(d):
   '''Make a confusion matrix. Rows contain the label, columns the prediction.'''
   result = []
-  hmd = DataSet(helpers.hard_max(dataset.xs), dataset.ys, None)
-  for ci in range(dataset.nclasses):
+  hmd = DataSet(helpers.hard_max(d.xs), d.ys, None)
+  for ci in range(d.nclasses):
     cid = hmd.get_class(ci)
     result.append(np.sum(cid.xs, axis=0))
   return np.array(result).astype(int)
 
-def format_confmat(dataset):
-  c = confusion_matrix(dataset)
+def format_confmat(d):
+  c = confusion_matrix(d)
   formatter = lambda items : '%10s' % items[0][:10] + '|' + \
     ''.join(['%8s' % i[:8] for i in items[1:]]) 
-  labels = [label[:6] for label in dataset.cl_lab]
+  labels = [label[:6] for label in d.cl_lab]
   hline = '-' * len(formatter([''] + labels))
   result = []
   
@@ -46,7 +46,7 @@ def auc(d):
 def mean_std(loss_f, ds):
   '''Calc mean and std for loss function loss_f over a list with DataSets ds'''
   losses = [loss_f(d) for d in ds]
-  return (np.mean(losses), np.std(losses))
+  return (np.mean(losses, axis=0), np.std(losses, axis=0))
 
 def I(d):
   '''Mutual information'''
