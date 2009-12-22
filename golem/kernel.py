a0c87e4b74ed510a0fbff1fb355b2ed4c1dd8c4c
Boris Reuderink <b.reuderink@gmail.com>
2009-12-22 15:07:15 +0100

    Simplified kernel calculation, potentially saved some memory.

--- golem/kernel.py
+++ golem/kernel.py
@@ -9,19 +9,19 @@ def build_kernel_matrix(instances_row, instances_col, kernel=None, **params):
   if kernel == None or kernel == 'linear':
     kernel_matrix = build_kernel_matrix(irow, icol, 'poly', degree=1)
   elif kernel=='poly':
-    assert(params['degree'] > 0)
-    assert(isinstance(params['degree'], int))
-    kernel_matrix = np.dot(irow, icol.T)
-    kernel_matrix = np.power(kernel_matrix, params['degree'])
+    assert params['degree'] > 0
+    assert isinstance(params['degree'], int)
+    kernel_matrix = np.dot(irow, icol.T) ** params['degree']
   elif kernel=='rbf':
-    assert(params['sigma'] > 0)
+    sigma = float(params['sigma'])
+    assert sigma > 0
     # calculate k(a, b) = \exp(-\frac{||a-b||^2}{(2 * \sigma^2)}
     # ||a-b||^2 = \sum(a_i-b_i)^2 = a \cdot a + b \cdot b - 2 (a \cdot b)
+    rrdot = np.sum(irow ** 2, axis=1).reshape(-1, 1)
+    ccdot = np.sum(icol ** 2, axis=1).reshape(1, -1)
     rcdot = np.dot(irow, icol.T)
-    rrdot = np.tile(np.sum(irow * irow, 1), (icol.shape[0], 1)).T
-    ccdot = np.tile(np.sum(icol * icol, 1), (irow.shape[0], 1))
-    kernel_matrix = rrdot + ccdot -2 * rcdot
-    return np.exp(-kernel_matrix/(2 * params['sigma'] ** 2))
+    kernel_matrix = -2 * rcdot + rrdot + ccdot
+    return np.exp(-kernel_matrix/(2 * sigma ** 2))
   else:
     # Manually fill kernel matrix with kernel function
     nrows, ncols = irow.shape[0], icol.shape[0]
