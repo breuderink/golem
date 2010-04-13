bef43d5d150ebe10b9f2cc7cc2265e3fb9fe5acc
Boris Reuderink <b.reuderink@gmail.com>
2010-04-13 13:48:50 +0200

    Removed typecasting from kernel; tests are clean now.

--- golem/kernel.py
+++ golem/kernel.py
@@ -3,32 +3,28 @@ import numpy as np
 
 def build_kernel_matrix(xs_row, xs_col, kernel=None, **params):
   '''Build Gramm-matrix or kernel-matrix'''
-  for xs in [xs_row, xs_col]:
-    assert xs.dtype in [np.float32, np.float64, np.int], \
-      'dtype %s is not supported in kernel' % xs.dtype
-  irow, icol = xs_row.astype(np.float32), xs_col.astype(np.float32)
-
   if kernel == None or kernel == 'linear':
-    kernel_matrix = build_kernel_matrix(irow, icol, 'poly', degree=1)
+    kernel_matrix = build_kernel_matrix(xs_row, xs_col, 'poly', degree=1)
   elif kernel=='poly':
-    assert params['degree'] > 0
-    assert isinstance(params['degree'], int)
-    kernel_matrix = np.dot(irow, icol.T) ** params['degree']
+    d = params['degree']
+    assert isinstance(d, int) and d > 0
+    kernel_matrix = np.dot(xs_row, xs_col.T) ** d
   elif kernel=='rbf':
     sigma = float(params['sigma'])
     assert sigma > 0
     # calculate k(a, b) = \exp(-\frac{||a-b||^2}{(2 * \sigma^2)}
     # ||a-b||^2 = \sum(a_i-b_i)^2 = a \cdot a + b \cdot b - 2 (a \cdot b)
-    rrdot = np.sum(irow ** 2, axis=1).reshape(-1, 1)
-    ccdot = np.sum(icol ** 2, axis=1).reshape(1, -1)
-    rcdot = np.dot(irow, icol.T)
+    rrdot = np.sum(xs_row ** 2, axis=1).reshape(-1, 1)
+    ccdot = np.sum(xs_col ** 2, axis=1).reshape(1, -1)
+    rcdot = np.dot(xs_row, xs_col.T)
     kernel_matrix = -2 * rcdot + rrdot + ccdot
     return np.exp(-kernel_matrix/(2 * sigma ** 2))
   else:
     # Manually fill kernel matrix with kernel function
-    nrows, ncols = irow.shape[0], icol.shape[0]
+    nrows, ncols = xs_row.shape[0], xs_col.shape[0]
     kernel_matrix = np.zeros((nrows, ncols))
     for r in xrange(nrows):
       for c in xrange(ncols):
-        kernel_matrix[r, c] = kernel(irow[r, :], icol[c, :])
+        kernel_matrix[r, c] = kernel(xs_row[r, :], xs_col[c, :])
+  assert kernel_matrix.dtype in (np.float32, np.float64)
   return kernel_matrix
--- golem/tests/testkernel.py
+++ golem/tests/testkernel.py
@@ -4,9 +4,6 @@ import numpy.linalg as la
 
 from ..kernel import build_kernel_matrix
 
-def test_kernel_function(k, f, x1, x2):
-  return np.allclose(k, build_kernel_matrix(x1, x2, f))
-
 def test_kernel_props(kernel_matrix):
   '''Test positive trace and symmetry of kernel matrix'''
   if not np.allclose(kernel_matrix, kernel_matrix.T):
@@ -40,43 +37,39 @@ class TestKernel(unittest.TestCase):
   def test_linear(self):
     '''Test linear kernel'''
     x1, x2 = self.x1, self.x2
-    k11 = build_kernel_matrix(x1, x1)
-    test_kernel_props(k11)
-    self.assert_(np.allclose(k11, build_kernel_matrix(x1, x1, 
-      lambda a, b: np.dot(a, b))))
+    test_kernel_props(build_kernel_matrix(x1, x1))
+    self.assert_(np.allclose(build_kernel_matrix(x1, x2), 
+      build_kernel_matrix(x1, x2, lambda a, b: np.dot(a, b))))
 
   def test_rbf(self):
     '''Test rbf kernel'''
-    x1, x2 = self.x1, self.x2
     def kernel(a, b, sigma):
       d = a - b
       return math.exp(-(np.dot(d, d))/(2. * sigma ** 2))
+    x1, x2 = self.x1, self.x2
 
     # Test different kernel sizes
     for s in [.1, 5, 20]:
-      k11 = build_kernel_matrix(x1, x1, 'rbf', sigma=s)
-      test_kernel_props(k11)
-      self.assert_(np.allclose(k11, 
-        build_kernel_matrix(x1, x1, lambda a, b: kernel(a, b, s))))
+      test_kernel_props(build_kernel_matrix(x1, x1, 'rbf', sigma=s))
+      self.assert_(np.allclose(build_kernel_matrix(x1, x2, 'rbf', sigma=s),
+        build_kernel_matrix(x1, x2, lambda a, b: kernel(a, b, s))))
 
     # Test that this kernel fails for sigma == 0
-    self.assertRaises(Exception, build_kernel_matrix, x1, x1, 'rbf', sigma = 0)
+    self.assertRaises(Exception, build_kernel_matrix, x1, x1, 'rbf', sigma=0)
 
   def test_poly(self):
     '''Test polynomial kernel'''
-    x1 = self.x1.astype(np.float32)
     def kernel(a, b, degree):
       return np.dot(a, b) ** float(degree)
+    x1, x2 = self.x1, self.x2
 
     # Test different kernel sizes
     for d in [1, 2, 3, 6]:
-      k11 = build_kernel_matrix(x1, x1, 'poly', degree=d)
-      test_kernel_props(k11)
-      self.assert_(np.allclose(k11, 
-        build_kernel_matrix(x1, x1, lambda a, b: kernel(a, b, d)).astype(np.float32)))
+      test_kernel_props(build_kernel_matrix(x1, x1, 'poly', degree=d))
+      self.assert_(np.allclose(build_kernel_matrix(x1, x2, 'poly', degree=d), 
+        build_kernel_matrix(x1, x2, lambda a, b: kernel(a, b, d))))
 
     # Test that this kernel failes for invalid degrees
-    self.assertRaises(Exception, build_kernel_matrix, 
-      x1, x1, 'poly', degree=0)
+    self.assertRaises(Exception, build_kernel_matrix, x1, x1, 'poly', degree=0)
     self.assertRaises(Exception, build_kernel_matrix, 
       x1, x1, 'poly', degree=1.5)
