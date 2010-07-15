fc2c0c241ca8912283c9bfb9708b0945e749e67d
Boris Reuderink <b.reuderink@gmail.com>
2010-07-15 10:19:37 +0200

    Factored cvxopt code in separate method.

--- golem/nodes/svm.py
+++ golem/nodes/svm.py
@@ -1,4 +1,5 @@
 # coding: utf8
+import logging
 import numpy as np
 import cvxopt.base as cvx
 import cvxopt.solvers
@@ -7,7 +8,45 @@ from basenode import BaseNode
 from ..kernel import build_kernel_matrix
 from ..dataset import DataSet
 
-ALPHA_RTOL = 1e-5
+def cvxopt_svm(K, labels, C):
+  # See "Learning with Kernels", Schölkopf and Smola, p205
+  ALPHA_RTOL = 1e-5
+
+  log = logging.getLogger('golm.cvxopt_svm')
+  assert np.all(np.unique(labels) == [-1, 1])
+  m = K.shape[0]
+  
+  log.debug('Creating QP-target')
+  # (4) min W(a) = -sum(a_i) + (1/2) * a' * Q * a
+  # -sum(a_i) = q'a
+  label_matrix = np.dot(labels, labels.T)
+  Q = cvx.matrix(K * label_matrix)
+  q = cvx.matrix([-1. for i in range(m)])
+
+  log.debug('Creating QP-constraints')
+  # (2) 0 < all alphas < C/m, using Ga <= h
+  # (3) sum(a_i * y_i) = 0, using Aa = b
+
+  # (2) is solved in two parts, first 0 < alphas, then alphas < C / m
+  G1 = cvx.spmatrix(-1, range(m), range(m))
+  G2 = cvx.spmatrix(1, range(m), range(m))
+  G = cvx.sparse([G1, G2])
+  h = cvx.matrix([0. for i in range(m)] + [float(C)/m for i in range(m)])
+  A = cvx.matrix(labels.T) # The deep copy is needed to prevent a TypeError
+  b = cvx.matrix(0.)
+
+  log.debug('Solving QP')
+  cvxopt.solvers.options['show_progress'] = False
+  sol = cvxopt.solvers.qp(Q, q, G, h, A, b)
+  if sol['status'] != 'optimal':
+    log.warning('QP solution status: ' + sol['status'])
+  log.debug('solver.status = ' + sol['status'])
+  alphas = np.array(sol['x'])
+
+  log.debug('Extracting Support Vectors')
+  # a_i gets close to zero, but does not always equal zero:
+  alphas[alphas < np.max(alphas) * ALPHA_RTOL] = 0
+  return alphas
 
 class SVM(BaseNode):
   def __init__(self, C=2, kernel=None, **params):
@@ -21,45 +60,16 @@ class SVM(BaseNode):
     xs, ys = d.xs, d.ys
     log = self.log
 
-    # See "Learning with Kernels", Schölkopf and Smola, p205
     log.debug('Calculate kernel matrix')
-    # linear kernel, so each entry contains the dot-product:
-    kernel_matrix = build_kernel_matrix(xs, xs, kernel=self.kernel, 
+    K = build_kernel_matrix(xs, xs, kernel=self.kernel, 
       **self.params)
-    m = d.ninstances
+    labels = (ys[:,0] - ys[:,1]).reshape(d.ninstances, 1).astype('d')
     
-    log.debug('Creating QP-target')
-    # (4) min W(a) = -sum(a_i) + (1/2) * a' * Q * a
-    # -sum(a_i) = q'a
-    labels = (ys[:,0] - ys[:,1]).reshape(m, 1).astype('d')
-    label_matrix = np.dot(labels, labels.T)
-    Q = cvx.matrix(kernel_matrix * label_matrix)
-    q = cvx.matrix([-1. for i in range(m)])
+    alphas = cvxopt_svm(K, labels, self.C)
 
-    log.debug('Creating QP-constraints')
-    # (2) 0 < all alphas < C/m, using Ga <= h
-    # (3) sum(a_i * y_i) = 0, using Aa = b
-
-    # (2) is solved in two parts, first 0 < alphas, then alphas < C / m
-    G1 = cvx.spmatrix(-1, range(m), range(m))
-    G2 = cvx.spmatrix(1, range(m), range(m))
-    G = cvx.sparse([G1, G2])
-    h = cvx.matrix([0. for i in range(m)] + [float(self.C)/m for i in range(m)])
-    A = cvx.matrix(labels.T) # The deep copy is needed to prevent a TypeError
-    b = cvx.matrix(0.)
-
-    log.debug('Solving QP')
-    cvxopt.solvers.options['show_progress'] = False
-    sol = cvxopt.solvers.qp(Q, q, G, h, A, b)
-    if sol['status'] != 'optimal':
-      log.warning('QP solution status: ' + sol['status'])
-    log.debug('solver.status = ' + sol['status'])
-    alphas = np.array(sol['x'])
-
-    log.debug('Extracting Support Vectors')
-    # a_i gets close to zero, but does not always equal zero
-    sv_ids = np.where(alphas >= np.max(alphas) * ALPHA_RTOL)[0]
-    log.info('Found %d SVs (%.2f%%)'% (len(sv_ids), len(sv_ids) * 100./m))
+    sv_ids = np.where(alphas != 0)[0]
+    log.info('Found %d SVs (%.2f%%)' % (len(sv_ids), 
+      len(sv_ids) * 100./d.ninstances))
     log.debug('Found SVs with indices: ' + str(sv_ids))
     
     model = {}
@@ -67,8 +77,8 @@ class SVM(BaseNode):
     model['SVs'] = xs[sv_ids, :]
     model['labels'] = np.array(labels)[sv_ids, :]
     
-    # Calculate b in f(x) = <w, x> + b
-    sv_kernel = np.array(kernel_matrix)[sv_ids,:][:, sv_ids]
+    # Calculate b in f(x) = wx + b
+    sv_kernel = np.array(K)[sv_ids,:][:, sv_ids]
     model['b'] = np.mean(model['labels'] - 
       np.dot(sv_kernel, (model['labels'] * model['alphas'])).T)
     self.model = model
@@ -79,9 +89,9 @@ class SVM(BaseNode):
     SVs, alphas = model['SVs'], model['alphas']
     labels, b = model['labels'], model['b']
 
-    kernel_matrix = build_kernel_matrix(xs, SVs, self.kernel, **self.params)
+    K = build_kernel_matrix(xs, SVs, self.kernel, **self.params)
     # Eq. 7.25: f(x) = sign(sum_i(y_i a_i k(x, x_i) + b), we do not sign!
-    labels = np.dot(kernel_matrix, (alphas * labels)) + b
+    labels = np.dot(K, (alphas * labels)) + b
 
     # Transform into two-colum hyperplane distance format
     labels = labels.reshape(-1, 1)
