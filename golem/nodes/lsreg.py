17558393397c9f8498eb9ffee7300b97c9405bd5
Boris Reuderink <b.reuderink@gmail.com>
2011-01-11 11:32:28 +0100

    Updated lsreg to new-style DataSet members.

--- golem/nodes/lsreg.py
+++ golem/nodes/lsreg.py
@@ -7,15 +7,16 @@ from basenode import BaseNode
 class LSReg(BaseNode):
   def train_(self, d):
     # Add 'offset-feature'
-    xs = np.hstack([d.xs, np.ones((d.ninstances, 1))])
+    X = np.vstack([d.X, np.ones(d.ninstances)])
     # Train least-squares
-    self.W, self.residual, rank, s = np.linalg.lstsq(xs, d.ys)
+    self.W, self.residual, _, _ = np.linalg.lstsq(X.T, d.Y.T)
+    self.W = self.W.T
     self.mse = self.residual / d.ninstances
     self.log.info('MSE = %s' % self.mse)
     self.cl_lab = d.cl_lab
 
   def apply_(self, d):
-    xs = np.hstack([d.xs, np.ones((d.ninstances, 1))])
-    xs = np.dot(xs, self.W)
-    return DataSet(xs, feat_lab=self.cl_lab, default=d)
+    X = np.vstack([d.X, np.ones(d.ninstances)])
+    X = np.dot(self.W, X)
+    return DataSet(X=X, feat_lab=self.cl_lab, default=d)
 
