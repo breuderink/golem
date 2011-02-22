bc13867049749040d41285b048351eaefe2a52a2
Boris Reuderink <b.reuderink@gmail.com>
2011-02-22 15:05:07 +0100

    Simplified artificial Gaussian dataset generation.

--- golem/data.py
+++ golem/data.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.linalg as la
 from dataset import DataSet
+from helpers import to_one_of_n
 
 def gaussian_dataset(ninstances=[50, 50]):
   mus = [\
@@ -19,6 +20,6 @@ def gaussian_dataset(ninstances=[50, 50]):
 
   for (ci, n) in enumerate(ninstances):
     Xs.append(np.random.multivariate_normal(mus[ci], sigmas[ci], n).T)
-    Ys.append(np.tile((np.arange(nclasses) == ci).astype(int), (n, 1)).T)
+    Ys.extend(np.ones(n) * ci)
 
-  return DataSet(X=np.hstack(Xs), Y=np.hstack(Ys))
+  return DataSet(X=np.hstack(Xs), Y=to_one_of_n(Ys))
